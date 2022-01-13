b54ff8bcbdb7aabbfd0290bc7e8cf515eed10996
Cjsah <2684117397@qq.com>
2022-01-14 00:33:28 +0800

    update

--- src/main/java/com/xekr/ironstars/blocks/entity/AbstractEnergyBlockEntity.java
+++ src/main/java/com/xekr/ironstars/blocks/entity/AbstractEnergyBlockEntity.java
@@ -1,13 +1,28 @@
 package com.xekr.ironstars.blocks.entity;
 
+import com.xekr.ironstars.capability.StarsEnergyCapability;
+import com.xekr.ironstars.registry.AllCapabilities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @Deprecated //TODO 先写着, 写完再去掉
 public class AbstractEnergyBlockEntity extends BlockEntity {
     protected AbstractEnergyBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+    }
+
+    @NotNull
+    @Override
+    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (cap == AllCapabilities.STARS_ENERGY_CAPABILITY)
+            return LazyOptional.of(StarsEnergyCapability::new).cast();
+        return super.getCapability(cap, side);
     }
 }
--- src/main/java/com/xekr/ironstars/capability/IStarsEnergyCapability.java
+++ src/main/java/com/xekr/ironstars/capability/IStarsEnergyCapability.java
@@ -1,9 +1,35 @@
 package com.xekr.ironstars.capability;
 
-public interface IStarsEnergyCapability {
-    /**发电效率 总电量*/
-//    int generationEfficiency;
-//    /**电源效率 用电量*/
-//    int powerEfficiency;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraftforge.common.util.INBTSerializable;
 
+public interface IStarsEnergyCapability extends INBTSerializable<CompoundTag> {
+    /**
+     * 获取发电效率
+     */
+    int getGenEfficiency();
+
+    /**
+     * 获取剩余效率
+     */
+    int getRestEfficiency();
+
+    /**
+     * 增加效率
+     * <p>只有发电机才会增加效率
+     */
+    void add(int amount);
+
+    /**
+     * 减少效率
+     */
+    void reduce(int amount);
+
+    /**
+     * 获取输出效率
+     * <p>如果效率不足, 降低效率输出
+     * @param expect 期待效率
+     * @return 实际效率
+     */
+    int getOutput(int expect);
 }
